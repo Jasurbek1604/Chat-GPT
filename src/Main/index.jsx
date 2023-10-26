460393bea42a61445f9779e359ce34f94c30bc31
jasurbek <jasurdev1604@gmail.com>
2023-10-26 15:01:37 +0500

    Side Bar

--- src/Main/index.jsx
+++ src/Main/index.jsx
@@ -1,11 +1,20 @@
 import React, { memo } from "react";
 import { Container } from "./style";
+import bars from "../assets/bars.svg";
+import x from "../assets/x-solid.svg";
 
 const Main = ({ value }) => {
   const [isOpen, setIsOpen] = value;
   return (
     <Container isOpen={isOpen}>
-      <button onClick={() => setIsOpen(!isOpen)}>click</button>
+      <div onClick={() => setIsOpen(!isOpen)}>
+        <img
+          src={isOpen ? x : bars}
+          alt=""
+          width={isOpen ? 20 : 30}
+          style={{ margin: "10px" }}
+        />
+      </div>
     </Container>
   );
 };
