c02f58154129f5714426ae78f8b7a7f6ecc8d7a0
jasurbek <jasurdev1604@gmail.com>
2023-10-26 15:45:38 +0500

    Main Bar

--- src/Main/index.jsx
+++ src/Main/index.jsx
@@ -1,7 +1,20 @@
 import React, { memo } from "react";
-import { Container, Wrapper, Message, Inputs, Menu, Bar } from "./style";
+import {
+  Container,
+  Wrapper,
+  Message,
+  MainBottom,
+  Menu,
+  Bar,
+  Form,
+  Input,
+  Button,
+  Img,
+  Text,
+} from "./style";
 import bars from "../assets/bars.svg";
 import x from "../assets/x-solid.svg";
+import send from "../assets/send.svg";
 
 const Main = ({ value }) => {
   const [isOpen, setIsOpen] = value;
@@ -12,7 +25,18 @@ const Main = ({ value }) => {
       </Menu>
       <Wrapper>
         <Message></Message>
-        <Inputs></Inputs>
+        <MainBottom>
+          <Form>
+            <Input type="text" placeholder="Send a message" />
+            <Button>
+              <Img src={send} alt="sent button" />
+            </Button>
+          </Form>
+          <Text>
+            ChatGPT may produce inaccurate information about people, place, or
+            facts. ChatGPT April 16 Version
+          </Text>
+        </MainBottom>
       </Wrapper>
     </Container>
   );
--- src/Main/style.js
+++ src/Main/style.js
@@ -17,11 +17,56 @@ export const Bar = styled.img`
 `;
 
 export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
   margin: 0 auto;
   width: 80%;
-  background: red;
   height: calc(100vh - 40px);
+  @media (max-width: 800px) {
+    width: 95%;
+  }
 `;
 
-export const Message = styled.div``;
-export const Inputs = styled.div``;
+export const Message = styled.div`
+  flex: 4;
+  background: red;
+  border-radius: 8px;
+`;
+export const MainBottom = styled.div`
+  flex: 1;
+`;
+
+export const Form = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  background: rgba(28, 30, 58, 1);
+  padding: 15px 20px;
+  border-radius: 8px;
+  margin-top: 5px;
+`;
+export const Input = styled.input`
+  width: 100%;
+  outline: none;
+  background: transparent;
+  border: none;
+  font-size: 1.7rem;
+  color: #fff;
+`;
+export const Button = styled.button`
+  width: 30px;
+  height: 30px;
+  border: none;
+  background: transparent;
+  cursor: pointer;
+`;
+export const Img = styled.img`
+  width: 100%;
+  height: 100%;
+`;
+export const Text = styled.div`
+  text-align: center;
+  margin-top: 20px;
+  font-size: 1.2rem;
+`;
